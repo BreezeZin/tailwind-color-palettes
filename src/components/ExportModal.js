6f58f8e864fc06f6bd69c8cd6f417a2b568d09d4
Jason Noble <jason345@gmail.com>
2019-10-27 15:10:08 +1000

    Changed notyf configuations for react as it was causing problems with SSR

--- src/components/ExportModal.js
+++ src/components/ExportModal.js
@@ -1,51 +1,61 @@
-import React, { useContext } from "react"
+import React, { Component } from "react"
 import Modal from "./Modal"
 import OutputCode from "./OutputCode"
 import copy from "copy-to-clipboard"
-import NotyfContext from "./NotyfContext"
+import { Notyf } from "notyf"
 import "notyf/notyf.min.css"
 
-function ExportModal({ output, closeModal }) {
-  const notyf = useContext(NotyfContext)
-  const modalFooter = (
-    <div className="flex-grow">
-      <button
-        className="button button--primary flex"
-        onClick={() => {
-          copy(output)
-          notyf.success("Copied!")
-        }}
-      >
-        Copy to clipboard
-      </button>
-    </div>
-  )
+class ExportModal extends Component {
+  state = { notyf: null }
 
-  return (
-    <div>
-      <Modal
-        title="Tailwind Configuration"
-        footer={modalFooter}
-        closeModal={closeModal}
-      >
-        <p className="mb-1">
-          Copy and paste the code into your Tailwind configuation file
-        </p>
-        <p className="mb-4">
-          For more information visit the{" "}
-          <a
-            href="https://tailwindcss.com/docs/configuration"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline focus:underline"
-          >
-            Tailwind CSS configuation guide
-          </a>
-        </p>
-        <OutputCode output={output} />
-      </Modal>
-    </div>
-  )
+  componentDidMount() {
+    this.setState({
+      notyf: new Notyf(),
+    })
+  }
+
+  render() {
+    const { output, closeModal } = this.props
+    const modalFooter = (
+      <div className="flex-grow">
+        <button
+          className="button button--primary flex"
+          onClick={() => {
+            copy(output)
+            this.state.notyf.success("Copied!")
+          }}
+        >
+          Copy to clipboard
+        </button>
+      </div>
+    )
+
+    return (
+      <div>
+        <Modal
+          title="Tailwind Configuration"
+          footer={modalFooter}
+          closeModal={closeModal}
+        >
+          <p className="mb-1">
+            Copy and paste the code into your Tailwind configuation file
+          </p>
+          <p className="mb-4">
+            For more information visit the{" "}
+            <a
+              href="https://tailwindcss.com/docs/configuration"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline focus:underline"
+            >
+              Tailwind CSS configuation guide
+            </a>
+          </p>
+          <OutputCode output={output} />
+        </Modal>
+      </div>
+    )
+  }
 }
 
 export default ExportModal
